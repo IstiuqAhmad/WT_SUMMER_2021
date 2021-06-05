5189b01d2099432fb87b20e90b6300303b9d904a
IstiuqAhmad <65025050+IstiuqAhmad@users.noreply.github.com>
2021-06-05 12:54:56 +0600

    Update File_Registration.php

--- Task_1/File_Registration.php
+++ Task_1/File_Registration.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
 
 <head>
     
@@ -13,56 +12,56 @@
                 <td><label for="name">Full Name: </label></td>
                 <td><input type="text" name="FullName"></td>
             </tr>
-            <br>
+                                 
             <tr>
                 <td><label for="email">Email: </label></td>
                 <td><input type="email"></td>
             </tr>
-            <br><br>
+                       
             <tr>
-                <td><label for="password">Password : </label></td>
+                <td><label for="password">Password: </label></td>
                 <td><input type="password"></td>
-            </tr>
-            <br>
+            </tr>          
+           
             <tr>
                 <td><label for="Comment">Comment:</label></td>
                 <td><textarea id="cmnt1" name="comment" rows="4" cols="40">
                 </textarea></td>
-            
             </tr>
-            
-            <br>
+                      
             <tr>
-                <td><label for="gender">Gender : </label></td>
-                <td><input type="radio" id="male" name="gender" value="male">Male</td>
-                <td><input type="radio" id="female" name="gender" value="female">Female</td>
-                <td><input type="radio" id="others" name="gender" value="others">Others</td>
-            </tr>
-          
-                <br>
+                <td><label for="gender">Gender: </label></td>
+                <td>
+                <input type="radio" id="male" name="gender" value="male"> Male 
+                <input type="radio" id="female" name="gender" value="female"> Female 
+                <input type="radio" id="others" name="gender" value="others"> Others 
+                </td>
+            </tr>  
+                      
             <tr>
                
-               <td><label for="hobby">Please Choose a hobby : </label></td>
-               <td><input type="checkbox" id="singing1" name="hobby1" value="Singing"></td>
-               <td><label for="hobby1">Singing</label>
-               <td><input type="checkbox" id="dancing2" name="hobby2" value="Dancing"></td>
-               <td><label for="hobby2">Dancing</label>
-               <td><input type="checkbox" id="reading3" name="hobby3" value="Reading"></td>
-               <td><label for="hobby3">Reading</label>        
+                <td><label for="hobby"> Please Choose a hobby: </label></td>
+                <td>
+                <input type="checkbox" id="singing1" name="hobby1" value="Singing">
+                <label for="hobby1"> Singing </label>
+                <input type="checkbox" id="dancing2" name="hobby2" value="Dancing">
+                <label for="hobby2"> Dancing </label>
+                <input type="checkbox" id="reading3" name="hobby3" value="Reading">
+                <label for="hobby3"> Reading </label>
+                </td>       
             </tr>
-            
-            <br>
+                     
             <tr>
-               <td><label for="hobby">Please Choose a File : </label></td>
+               <td><label for="hobby">Please Choose a File: </label></td>
                <td><input type="file" id="myfile" name="myfile"></td>
               
-             
             </tr>  
-            
-            <br>
+       
             <tr>
-                <td><input type="submit" value="Submit"></td>
-                <td><input type="reset"></td>
+                <td>
+                <input type="submit" value="Submit">
+                <input type="reset">
+                </td>
             </tr>
         </table>
     </form>
